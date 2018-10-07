92fde661a8ac0936a9c791178549570c38443c57
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-10-07 10:01:38 -0400

    changed git ignore, added second heuristic

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def depth_first(initial_state):
 	
 	while(open_l):
 		x = open_l.pop()
-		print(x[1])
 		move_buffer.append((x[0], str(x[1])))
 		if x[1].goal_reached(): 
 			return move_buffer
@@ -61,6 +60,10 @@ def a_star(initial_state, h_func):
 	return []
 
 def manh_dist(state):
+	"""
+	First heuristic function, computes the Manhattan distance in terms of the array indices as x-y coordinates
+	"""
+
 	score = 0
 	goal_states = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]]
 	goal_coord = dict( (j,(x, y)) for x, i in enumerate(goal_states) for y, j in enumerate(i) )
@@ -71,6 +74,26 @@ def manh_dist(state):
             	b = goal_coord[tile]
 
             	score += abs(a[0]-b[0]) + abs(a[1]-b[1])
+	return score
+
+def row_col_diff(state):
+	"""
+	Second heuristic function, computes the number of tiles out of row plus the number of tiles out of column
+	"""
+
+	score = 0
+	goal_states = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]]
+	goal_coord = dict( (j,(x, y)) for x, i in enumerate(goal_states) for y, j in enumerate(i) )
+
+	for row_num, row in enumerate(state):
+            for col_num, tile in enumerate(row):
+            	a = (row_num, col_num)
+            	b = goal_coord[tile]
+
+            	if a[0] != b[0]:
+            		score += 1
+            	if a[1] != b[1]:
+            		score += 1
 	return score
 
 def test():
@@ -96,7 +119,7 @@ for i in raw_state:
 #initial_state = [[1,2,3,4],[5,6,8,7],[9,10,0,11]]
 initial_state = [[1, 0, 3, 7], [5, 2, 6, 4], [9, 10, 11, 8]] 
 #print(depth_first(initial_state))
-print(best_first(initial_state, manh_dist))
+print(best_first(initial_state, row_col_diff))
 
 
 
